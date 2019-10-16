da3f4e1947ff2ea47754d46a567e1c9e56ec9b59
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 13:50:07 +0200

    Updates

---
+++ precache-manifest.550eff38a36d508ba444599779f0eceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3019594bce348e9ebcbdf86814119c40",
+    "url": "/My-website-react/index.html"
+  },
+  {
+    "revision": "201604d660225519e1b4",
+    "url": "/My-website-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "37103e6979c9439ccef5",
+    "url": "/My-website-react/static/js/2.c69504d0.chunk.js"
+  },
+  {
+    "revision": "201604d660225519e1b4",
+    "url": "/My-website-react/static/js/main.df6a1e16.chunk.js"
+  },
+  {
+    "revision": "5eb05fd8c8dff9451373",
+    "url": "/My-website-react/static/js/runtime-main.02b3a997.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/My-website-react/static/media/logo.25bf045c.svg"
+  }
+]);
